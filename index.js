caf4a7d8e183215f2814283356891790c8f847f0
anusha-nemani <102588291+anusha-nemani@users.noreply.github.com>
2022-12-19 11:28:45 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+let player = {
+  name: "John",
+  chips: 145,
+};
+
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+let newGameEl = document.getElementById("newGame-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+
+  if (randomCard === 1) {
+    return 11;
+  } else if (randomCard >= 11) {
+    return 10;
+  } else {
+    return randomCard;
+  }
+}
+
+function startGame() {
+  newGameEl.textContent = "";
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  isAlive = true;
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard];
+  renderGame();
+}
+
+function renderGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+    newGameEl.textContent = "Click on START GAME to start a new game";
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+    newGameEl.textContent = "Click on START GAME to start a new game";
+  }
+
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: ";
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+  } else {
+    newGameEl.textContent = "Click on START GAME to start a new game";
+  }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+- === is strictly equal operator
+
+- == will give true if we check for a string and an integer
+  for example, 100 == "100" will return True, whereas === will give False
+
+- we can use querySelector to get elements from the website. in this, we specify if it is id or class by using # or . respectively before the name
